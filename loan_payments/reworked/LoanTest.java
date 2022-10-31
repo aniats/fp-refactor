e529815bf734918954ff7254d885d204cf522359
Ania Tselikova <ania.tselikova@gmail.com>
2022-10-31 19:34:56 +0300

    fix: adjust tests to our refactoring

--- loan_payments/reworked/LoanTest.java
+++ loan_payments/reworked/LoanTest.java
@@ -3,15 +3,15 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.*;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.time.Instant;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Optional;
 
-import static org.mockito.Mockito.*;
-
-public class Loan_processTest{
+public class LoanTest{
 
     private InputStream storedInput;
 
@@ -26,14 +26,12 @@ public class Loan_processTest{
     }
 
     @Test
-    public void get_infoTest() throws Exception {
-
+    public void parseInfoTest() throws Exception {
         String input = "10.10.2017\n24 10000.0 10.0\n";
 
         InputStream inputStream = new ByteArrayInputStream(input.getBytes());
-        System.setIn(inputStream);
 
-        LoanInfo info = Loan_process.get_info(System.in, Optional.empty());
+        LoanInfo info = OutputAdapter.parseInfo(inputStream, Optional.empty());
 
         DateFormat df = new SimpleDateFormat("dd.MM.yyyy", Locale.ENGLISH);
         Assert.assertEquals(df.parse("10.10.2017"), info.loan_date);
@@ -44,23 +42,11 @@ public class Loan_processTest{
     }
 
     @Test
-    public void addMonthTest() throws Exception {
-        DateFormat df = new SimpleDateFormat("dd.MM.yyyy", Locale.ENGLISH);
-        Date date = df.parse("11.05.2017");
-
-        Assert.assertEquals(df.parse("11.08.2017"), Loan_process.addMonth(date, 3));
-    }
-
-    @Test
     public void get_next_Schedule() throws Exception {
-        LoanInfo loanInfo = new LoanInfo();
         DateFormat df = new SimpleDateFormat("dd.MM.yyyy", Locale.ENGLISH);
-        loanInfo.loan_date = df.parse("11.05.2017");
-        loanInfo.amount = 24000.0;
-        loanInfo.months = 24;
-        loanInfo.percent = 12.0;
+        LoanInfo loanInfo = new LoanInfo(24, df.parse("11.05.2017"), 12.0, 24000.0, 100);
 
-        Schedule schedule = Loan_process.get_next_Schedule(4, loanInfo);
+        Schedule schedule = Loan.get_next_Schedule(4, loanInfo);
 
         Assert.assertEquals(df.parse("11.09.2017"), schedule.getDate());
         Assert.assertEquals(1000.0, schedule.getAmount_loan(), 0.001);
@@ -69,16 +55,13 @@ public class Loan_processTest{
 
     @Test
     public void get_next_ScheduleTest() throws Exception {
-        LoanInfo loanInfo = new LoanInfo();
         DateFormat df = new SimpleDateFormat("dd.MM.yyyy", Locale.ENGLISH);
-        loanInfo.loan_date = df.parse("11.05.2017");
-        loanInfo.amount = 24000.0;
-        loanInfo.months = 6;
-        loanInfo.percent = 12.0;
+        LoanInfo loanInfo = new LoanInfo(6, df.parse("11.05.2017"), 12.0, 24000.0, 100);
 
-        ArrayList<Schedule> schedules = Loan_process.getScheduleList(loanInfo);
+        ArrayList<Schedule> schedules = Loan.getScheduleList(loanInfo);
 
         ArrayList<Schedule> idealSchedule = new ArrayList<>();
+
         idealSchedule.add(new Schedule(df.parse("11.05.2017"), 4000.0, 240.0));
         idealSchedule.add(new Schedule(df.parse("11.06.2017"), 4000.0, 200.0));
         idealSchedule.add(new Schedule(df.parse("11.07.2017"), 4000.0, 160.0));
