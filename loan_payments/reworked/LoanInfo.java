714b8e9c3831566455f1a764ed40acc8637da3f2
Ania Tselikova <ania.tselikova@gmail.com>
2022-10-31 19:21:01 +0300

    chore: create target folder with to-be-refactored code

---
+++ loan_payments/reworked/LoanInfo.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class LoanInfo {
+    public int months;
+    public Date loan_date;
+    public double percent;
+    public double amount;
+    public double payoutFine;
+}
